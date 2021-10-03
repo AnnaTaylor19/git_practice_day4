f9382b1a98f045e41b4d4fe3f8ef422727faa4ef
Anna Taylor <evergreen19@yandex.ru>
2021-10-03 12:24:28 -0400

    updated second file

--- src/setup/Second.java
+++ src/setup/Second.java
@@ -4,14 +4,15 @@ public class Second {
 
 
     public void m (){
-
+        System.out.println("first method");
     }
 
     public void m (int a){
+        System.out.println("second method");
 
     }
     public void m (String s){
-
+        System.out.println("third method");
     }
 
     //need to commit one more time
