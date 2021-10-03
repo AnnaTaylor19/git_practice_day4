39a3094042d47cf01787815bb7f92728d56892d7
Anna Taylor <evergreen19@yandex.ru>
2021-10-03 15:38:45 -0400

    made car in master branch

---
+++ src/setup/Car.java
@@ -0,0 +1,10 @@
+package setup;
+
+public class Car {
+
+    //this is xar class from master branch
+
+    //we will have conflict
+
+    // because there is different text in different branches for car
+}
